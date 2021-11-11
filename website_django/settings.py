9f891a1da82fe49bb8e4bc903252874d838e1894
iPasar Indonesia <puput@MacBook-Air.local>
2021-11-11 09:22:55 +0700

    create deploy

--- website_django/settings.py
+++ website_django/settings.py
@@ -15,6 +15,8 @@ import os
 import urllib
 from urllib.parse import quote
 import pymongo
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -152,3 +154,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
